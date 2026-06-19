8b218d039ea0581c72e5a3624cad232d48f48a8b
Boris Ngam <boris@deltavia.com>
2026-06-19 10:06:23 +0200

    refactor todos provider

--- 01-my-vue-app/src/modules/todos/pages/TodoBoard.vue
+++ 01-my-vue-app/src/modules/todos/pages/TodoBoard.vue
@@ -1,26 +1,15 @@
 <script setup>
-import { ref, computed, onMounted, inject } from 'vue'
+import { onMounted, inject } from 'vue'
 import TodoForm from '../components/TodoForm.vue'
 import TodoList from '../components/TodoList.vue'
 
-let id = 0
-const { todos, loadTodos } = inject('todos')
-const loading = ref(false); // TODO: implement loading state
-const error = ref(''); // TODO: implement error state
+const { todos, loading, error, loadTodos, addTodo, removeTodo } = inject('todos')
 
 onMounted(() => {
   setTimeout(() => {
     loadTodos()
   }, 3000) // simulate a delay for loading
 })
-
-// TODO: set in provider
-function addTodo(value) {
-  todos.value.push({ id: id++, todo: value, completed: false })
-}
-function removeTodo(todoId) {
-  todos.value = todos.value.filter((t) => t.id !== todoId)
-}
 
 </script>
 
--- 01-my-vue-app/src/modules/todos/providers/TodosProvider.vue
+++ 01-my-vue-app/src/modules/todos/providers/TodosProvider.vue
@@ -1,27 +1,64 @@
 <!-- inside provider component -->
 <script setup lang="ts">
 import { provide, ref } from 'vue'
+import type { Todo } from '../models/todo'
 
-const todos = ref([])
+const todos = ref<Todo[]>([])
+const loading = ref(false)
+const error = ref('')
+let nextId = 0
 
 function loadTodos(reload = false) {
-  // logic to load todos
   if(reload || todos.value.length === 0) {
+    loading.value = true
+    error.value = ''
+
     fetch('https://dummyjson.com/todos?limit=5')
       .then((response) => response.json())
       .then((data) => {
         todos.value = data.todos
+        // Set nextId based on highest existing id
+        if (data.todos.length > 0) {
+          nextId = Math.max(...data.todos.map((t: any) => t.id)) + 1
+        }
       })
       .catch((err) => {
-        alert('Failed to load todos');
+        error.value = 'Failed to load todos'
         console.error('Failed to load todos', err)
       })
+      .finally(() => {
+        loading.value = false
+      })
+  }
+}
+
+function addTodo(value: string) {
+  todos.value.push({
+    id: nextId++,
+    todo: value,
+    completed: false
+  })
+}
+
+function removeTodo(todoId: number) {
+  todos.value = todos.value.filter((t: any) => t.id !== todoId)
+}
+
+function toggleTodo(todoId: number) {
+  const todo = todos.value.find((t: any) => t.id === todoId)
+  if (todo) {
+    todo.completed = !todo.completed
   }
 }
 
 provide('todos', {
   todos,
-  loadTodos
+  loading,
+  error,
+  loadTodos,
+  addTodo,
+  removeTodo,
+  toggleTodo
 })
 </script>
 
